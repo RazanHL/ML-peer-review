4485d37bddc8ed36f709778980cecc0356f3bff9
Razan Hallak <razanhalla@gmail.com>
2023-07-30 12:18:25 +0300

    Modefications

--- Journal/settings.py
+++ Journal/settings.py
@@ -98,9 +98,17 @@ WSGI_APPLICATION = 'Journal.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',#_psycopg2',
+        'NAME': config('REMOTEDATABASE_NAME'),
+        'USER': config('REMOTEDATABASE_USER'), 
+        'PASSWORD': config('REMOTEDATABASE_PASSWORD'),
+        'HOST': config('REMOTEDATABASE_HOST'),
+        'PORT': config('REMOTEDATABASE_PORT'),
     }
 }
 
